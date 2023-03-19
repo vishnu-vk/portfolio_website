5cf0366ac86e2dc62a4eddb36f68a7e528cb4f7a
Vishnu <33258967+vishnu-vk@users.noreply.github.com>
2023-03-19 19:24:14 +0530

    updated cv

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,6 @@ import personalImg from './image.png'
 import notabookImg from './notabookImg.png'
 import portfolioImg from './portfolioImg.png'
 import arcanaImg from './arcanaImg.png'
-import resume from './sample.pdf'
+import resume from './VISHNU_K.pdf'
 
 export {heroImg, personalImg, notabookImg, portfolioImg, arcanaImg,resume}
