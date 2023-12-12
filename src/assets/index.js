a9b1adb388efe475d2fe6c954b95650cbc7eebc1
Vishnu <33258967+vishnu-vk@users.noreply.github.com>
2023-12-12 09:18:59 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,7 @@ import personalImg from './image.png'
 import notabookImg from './notabookImg.png'
 import portfolioImg from './portfolioImg.png'
 import arcanaImg from './arcanaImg.png'
+import cryptoImg from './cryptoImg.png'
 import resume from './VISHNU_K.pdf'
 
-export {heroImg, personalImg, notabookImg, portfolioImg, arcanaImg,resume}
+export {heroImg, personalImg, notabookImg, portfolioImg, arcanaImg, resume, cryptoImg}
